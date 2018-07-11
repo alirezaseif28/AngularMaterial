8e1fd0cd8b2d7907c2f5b9516f708b6d2faf4b79
ars791 <alirezaseif28@gmail.com>
2018-07-12 00:47:18 +0430

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MaterialModule } from "./material.module";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpClientModule,
+        MaterialModule
+    ],
+    entryComponents: [
+        // All components about to be loaded "dynamically" need to be declared in the entryComponents section.
+    ],
+    declarations: [
+        // common and shared components/directives/pipes between more than one module and components will be listed here.
+    ],
+    exports: [
+        // common and shared components/directives/pipes between more than one module and components will be listed here.
+        CommonModule,
+        FormsModule,
+        HttpClientModule,
+        MaterialModule
+    ]
+    /* No providers here! Since they’ll be already provided in AppModule. */
+})
+export class SharedModule {
+    static forRoot(): ModuleWithProviders {
+        // Forcing the whole app to use the returned providers from the AppModule only.
+        return {
+            ngModule: SharedModule,
+            providers: [ /* All of your services here. It will hold the services needed by `itself`. */]
+        };
+    }
+}
